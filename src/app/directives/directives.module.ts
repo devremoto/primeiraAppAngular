12493d16075e8f7aa2c5d59ebe71447cc4da52d7
Adilson de Almeida Pedro <adilson@almeidapedro.com.br>
2017-12-04 09:36:41 -0200

    criando diretivas em angular

--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { MarcaTextoDirective } from './marca-texto.directive';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [
-    MarcaTextoDirective
-  ],
-  exports: [
-    MarcaTextoDirective
-  ]
+  declarations: [MarcaTextoDirective],
+  exports: [MarcaTextoDirective]
 })
 export class DirectivesModule { }
